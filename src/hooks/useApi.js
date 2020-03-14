3cf877b5dfcf95ea50581afdf6800be8dbc3c7eb
lindastorgard <linda.storgard@gmail.com>
2020-03-14 14:39:50 +0100

    typo

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useAPI = productId => {
+const useAPI = () => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
