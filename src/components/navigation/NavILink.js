8af0356553cc88a68e3b1786359711d983eb8255
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-03-15 19:46:02 +0100

    style navlinks

--- src/components/navigation/NavILink.js
+++ src/components/navigation/NavILink.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'react-dom';
-import { PORTFOLIO, SERVICES, ABOUT, VIDEO, LANDING_PAGE } from '../urlRoutes';
+import { Link } from 'react-router-dom';
 
-const NavItem = styled(Link)`
-    list-style: none;
-    text-decoration: none;
+const Background = styled.div`
+    position: absolute;
+    left: -100px;
+    width: 100px;
+    height: 100%;
+    background-image: linear-gradient(to right, #ebe0e0, #fff);
+    transition: 1s;
+`;
+const Wrapper = styled.div`
+    position: relative;
+    overflow: hidden;
+    width: 150px;
+    height: 24px;
+    &:hover {
+        ${Background} {
+            transition: 1s;
+            left: 0;
+        }
+    }
 `;
 
-const NavLink = () => {
-    return <NavItem to={PORTFOLIO.path}>Portfolio</NavItem>;
+const NavItem = styled(Link)`
+    margin-top: 8px;
+    position: absolute;
+    list-style: none;
+    text-decoration: none;
+    font-size: ${({ theme }) => theme.fontSizes[2]};
+    font-family: ${({ theme }) => theme.fonts.body};
+    font-weight: ${({ theme }) => theme.fontWeights[0]};
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.text};
+    /* &:hover {
+        background-image: linear-gradient(to right, #ebe0e0, #fff);
+    } */
+`;
+
+const NavLink = ({ url, link }) => {
+    return (
+        <Wrapper>
+            <Background />
+            <NavItem to={url}>{link}</NavItem>
+        </Wrapper>
+    );
+};
+export default NavLink;
+
+NavLink.propTypes = {
+    url: PropTypes.string,
+    link: PropTypes.string,
 };
