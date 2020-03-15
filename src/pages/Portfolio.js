951cbd893c64fd78c89f422e8db72c8e90254841
Fanny Schriwer <fanny_schriwer@hotmail.com>
2020-03-15 19:39:22 +0100

    add flex layout for projects page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,11 +2,11 @@ import React from 'react';
 import useApi from '../hooks/useApi';
 import styled from 'styled-components';
 import { StyledH1 } from '../styles/typography';
-import useLocalStorage from '../hooks/useLocalStorage';
 import { Link } from 'react-router-dom';
 import { PROJECT_WITH_CATEGORY } from '../utils/urlRoutes';
 
 const Container = styled.div`
+    margin-left: 0;
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: repeat(2, 50vh);
@@ -14,15 +14,15 @@ const Container = styled.div`
     ${({ theme }) => theme.sm`
 		  grid-template-columns: repeat(2, 1fr);
 		  grid-template-rows: 100vh;
-		  grid-gap: ${({ theme }) => theme.space[2]};
+          grid-gap: ${({ theme }) => theme.space[2]};
+          margin-left: 250px;
 		`};
 `;
 
 const Title = styled(StyledH1)`
-    font-family: ${({ theme }) => theme.fonts.heading};
-    font-weight: 500;
     text-transform: uppercase;
     color: ${({ theme }) => theme.primary};
+    text-align: center;
 `;
 
 const CategorySection = styled.div`
@@ -38,6 +38,9 @@ const CategorySection = styled.div`
     &:nth-of-type(2) {
         grid-column: 1/2;
     }
+    a {
+        text-decoration: none;
+    }
     ${({ theme }) => theme.sm`
 		  &:first-of-type {
         grid-column: 1/2;
@@ -45,17 +48,11 @@ const CategorySection = styled.div`
     &:nth-of-type(2) {
         grid-column: 2/3;
     }
-		`};
-`;
-
-const ButtonWrapper = styled.button`
-    background: transparent;
-    border: none;
+	`};
 `;
 
 const Portfolio = () => {
     const { categories, error, isLoading } = useApi();
-    const [categoryFilter, setCategoryFilter] = useLocalStorage('category');
 
     return (
         <div>
@@ -70,9 +67,7 @@ const Portfolio = () => {
                         return (
                             <CategorySection key={acf.image.id} image={acf.image.url}>
                                 <Link to={`${PROJECT_WITH_CATEGORY.getPathWithId(acf.slug)}`}>
-                                    <ButtonWrapper onClick={() => setCategoryFilter(acf.slug)}>
-                                        <Title>{acf.category_name}</Title>
-                                    </ButtonWrapper>
+                                    <Title>{acf.category_name}</Title>
                                 </Link>
                             </CategorySection>
                         );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,58 @@ import React from 'react';
 import useApi from '../hooks/useApi';
 import styled from 'styled-components';
 import { Link, useParams } from 'react-router-dom';
+import { StyledH1 } from '../styles/typography';
+
+const Container = styled.div`
+    margin-left: 0;
+    ${({ theme }) => theme.xs`
+        margin-left: 250px;
+	`};
+`;
+
+const StyledSection = styled.section`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+    ${({ theme }) => theme.xs`
+        flex-direction: row;
+    `};
+`;
+
+const Title = styled(StyledH1)`
+    text-transform: uppercase;
+    text-align: center;
+    color: ${({ theme }) => theme.primary};
+`;
+
+const CategorySection = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: ${({ theme }) => theme.space[0]};
+    width: 100%;
+    height: 280px;
+    background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+        ${props => `url(${props.image}) no-repeat center`};
+    a {
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        height: 100%;
+        justify-content: center;
+        width: 100%;
+        &:hover {
+            background: rgba(0, 0, 0, 0.4);
+        }
+    }
+    ${({ theme }) => theme.sm`
+        width: 45%;
+    `};
+    ${({ theme }) => theme.md`
+        width: 30%;
+    `};
+`;
 
 const Projects = () => {
     const param = useParams();
@@ -9,17 +61,26 @@ const Projects = () => {
     console.log(category);
 
     return (
-        <div>
+        <Container>
             {isLoading ? (
                 <p>Loading...</p>
             ) : error ? (
                 <p>{error}...</p>
             ) : category ? (
-                category.map(project => {
-                    return <div key={project.id}>{project.acf.title}</div>;
-                })
+                <StyledSection>
+                    {category.map(project => {
+                        const { acf } = project;
+                        return (
+                            <CategorySection key={project.id} image={acf.image[0].url}>
+                                <Link to={`/`}>
+                                    <Title>{acf.title}</Title>
+                                </Link>
+                            </CategorySection>
+                        );
+                    })}
+                </StyledSection>
             ) : null}
-        </div>
+        </Container>
     );
 };
 export default Projects;
