b916d668ef7553fd994b2dd9545429ec98451ad6
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-03-23 22:40:00 +0100

    add grid layout for gallery

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -4,19 +4,50 @@ import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 
 const Container = styled.div`
-    margin-left: 0;
+    margin-right: ${({ theme }) => theme.space[1]};
+    margin-left: ${({ theme }) => theme.space[1]};
     margin-top: 80px;
     ${({ theme }) => theme.sm`
-        margin-left: 250px;
+        margin-left: 200px;
         margin-top: 0;
 	`};
+`;
+
+const GalleryWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: minmax(100px, auto);
+    grid-gap: ${({ theme }) => theme.space[1]};
+    ${({ theme }) => theme.sm`
+         grid-template-columns: repeat(3, 1fr);
+	`};
+`;
+const ImageWrapper = styled.div`
+    grid-column: span 1;
+    min-height: 100px;
+    &:nth-of-type(5n + 5) {
+        grid-column: 1 / 3;
+    }
+    ${({ theme }) => theme.sm`
+        grid-column: span 1;
+        min-height: 300px;
+        &:nth-of-type(5n + 5) {
+          grid-column: 2 / 4;
+        }
+	`};
+`;
+
+const Image = styled.img`
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
 `;
 
 const ProjectsDetails = () => {
     const { id } = useParams();
     const { project, error, isLoading } = useApi('', id);
     if (project) {
-        console.log(project[0].acf);
+        console.log(project[0].acf.image);
     }
 
     return (
@@ -26,7 +57,16 @@ const ProjectsDetails = () => {
             ) : error ? (
                 <p>{error}...</p>
             ) : project ? (
-                <h3>{project[0].acf.title}</h3>
+                <>
+                    <h3>{project[0].acf.title}</h3>
+                    <GalleryWrapper>
+                        {project[0].acf.image.map(({ url, id, title }) => (
+                            <ImageWrapper>
+                                <Image src={url} key={id} alt={title} />
+                            </ImageWrapper>
+                        ))}
+                    </GalleryWrapper>
+                </>
             ) : null}
         </Container>
     );
