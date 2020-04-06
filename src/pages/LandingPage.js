76a84ccdefb0676f232b0d8c2bcab890b5e02078
lindastorgard <linda.storgard@gmail.com>
2020-04-06 21:51:22 +0200

    add call to action button

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,8 +5,11 @@ import styled from 'styled-components';
 import Slideshow from '../components/Slideshow';
 import CircleLoader from '../components/CircleLoader';
 import { StyledParagraph } from '../styles/typography';
+import { PORTFOLIO } from '../utils/urlRoutes';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
+    position: relative;
     height: calc(100vh - 80px);
     margin-top: 80px;
     ${({ theme }) => theme.sm`
@@ -20,6 +23,26 @@ const Container = styled.div`
     `};
 `;
 
+// const Parent = styled.div`
+//     position: relative;
+// `;
+
+const StyledButton = styled.button`
+    position: absolute;
+    z-index: 100;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: ${({ theme }) => theme.fonts.heading};
+    font-size: ${({ theme }) => theme.fontSizes[4]};
+    text-transform: uppercase;
+    border: 1px solid ${({ theme }) => theme.primary};
+    opacity: 0.6;
+    background-color: ${({ theme }) => theme.secondary};
+    color: ${({ theme }) => theme.primary};
+    padding: ${({ theme }) => theme.space[1]} ${({ theme }) => theme.space[2]};
+`;
+
 const LandingPage = () => {
     const { landingpage, error, isLoading } = useApi();
     setScroll();
@@ -31,7 +54,14 @@ const LandingPage = () => {
             ) : error ? (
                 <StyledParagraph>{error}</StyledParagraph>
             ) : landingpage ? (
-                <Slideshow landingpage={landingpage} />
+                <>
+                    <Link to={PORTFOLIO.path} link="Portfolio">
+                      {/* <Parent> */}
+                          <StyledButton>Velkommen</StyledButton>
+                      {/* </Parent> */}
+                    </Link>
+                    <Slideshow landingpage={landingpage} />
+                </>
             ) : null}
         </Container>
     );
