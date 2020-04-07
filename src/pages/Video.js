fdee4f049275839082bcbb6567540d200e90c44b
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-04-07 17:21:21 +0200

    add styling

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import kitchen from '../assets/kitchen.png';
+import { PORTFOLIO } from '../utils/urlRoutes';
+import { Link } from 'react-router-dom';
+import { StyledH2 } from '../styles/typography';
 
 import styled from 'styled-components';
 
-const VideoContainer = styled.video`
+const HeaderTextContainer = styled.div`
+    position: absolute;
+    z-index: 100;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    h2 {
+        color: ${({ theme }) => theme.primary};
+        font-size: ${({ theme }) => theme.fontSizes[5]};
+        text-transform: uppercase;
+    }
+`;
+
+const StyledButton = styled.button`
+    font-family: ${({ theme }) => theme.fonts.body};
+    font-size: ${({ theme }) => theme.fontSizes[3]};
+    text-transform: uppercase;
+    border: 1px solid ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.primary};
+    background: transparent;
+    padding: ${({ theme }) => theme.space[1]} ${({ theme }) => theme.space[2]};
+`;
+const Overlay = styled.div`
+    /* opacity: 0.6;
+    background-color: ${({ theme }) => theme.secondary};
     position: fixed;
     right: 0;
+    left: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%; */
+`;
+
+const VideoContainer = styled.video`
+    z-index: 10;
+    position: fixed;
+    right: 0;
+    left: 0;
     bottom: 0;
     min-width: 100%;
     min-height: 100%;
@@ -13,9 +53,17 @@ const VideoContainer = styled.video`
 const Video = () => {
     return (
         <div>
-            <VideoContainer autoPlay loop muted playsInline>
-                <source src="https://eskeprosjekt.no/video/tapet.mp4" type="video/mp4" />
-            </VideoContainer>
+            <Link to={PORTFOLIO.path} link="Portfolio">
+                <HeaderTextContainer>
+                    <StyledH2>Eske Prosjekt er forlengelsen av designuniverset Eske Interiør </StyledH2>
+                    <StyledButton>Kom inn</StyledButton>
+                </HeaderTextContainer>
+            </Link>
+            <Overlay>
+                <VideoContainer autoPlay loop muted playsInline poster={kitchen}>
+                    <source src="https://eskeprosjekt.no/video/tapet.mp4" type="video/mp4" />
+                </VideoContainer>
+            </Overlay>
         </div>
     );
 };
