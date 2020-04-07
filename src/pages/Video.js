b8e3b1ee923fa37804adadaa22017705238d19e5
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-04-07 15:49:02 +0200

    add background video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import useApi from '../hooks/useApi';
-import { StyledH3 } from '../styles/typography';
-import Layout from '../components/Layout';
 
-function Video() {
-    const { data } = useApi();
-    if (data) {
-    }
+import styled from 'styled-components';
+
+const VideoContainer = styled.video`
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+`;
+
+const Video = () => {
     return (
-        <Layout>
-            <StyledH3>I'm Video</StyledH3>
-        </Layout>
+        <div>
+            <VideoContainer autoPlay loop muted playsInline>
+                <source src="https://eskeprosjekt.no/video/tapet.mp4" type="video/mp4" />
+            </VideoContainer>
+        </div>
     );
-}
+};
 
 export default Video;
