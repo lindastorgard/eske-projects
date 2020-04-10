36c4accc36b4aee0c48ab508eabf4c4d9f26015d
lindastorgard <linda.storgard@gmail.com>
2020-04-10 16:59:50 +0200

    add link to terms in cookie bar

--- src/components/ConsentCookie.js
+++ src/components/ConsentCookie.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CookieConsent from 'react-cookie-consent';
+import { Link } from 'react-router-dom';
+// import { TERMS } from '../utils/urlRoutes';
 
 const ConsentCookie = () => {
     return (
@@ -14,7 +16,10 @@ const ConsentCookie = () => {
             <span style={{ fontSize: '10px', fontFamily: 'Roboto Condensed' }}>
                 Nettstedet bruker cookies for å bedre møte dine forventninger. Hvis du ikke aksepterer cookies, kan du
                 deaktivere den i innstillingene til nettleseren din. Men det er en risiko for at enkelte funksjoner på
-                www.eskeinterior.no ikke vil fungere korrekt.
+                www.eskeinterior.no ikke vil fungere korrekt. &nbsp;
+                {/* <Link style={{ fontSize: '10px', fontFamily: 'Roboto Condensed', color: '#C199A6' }} to={TERMS.path} */}
+                    {/* Personvern */}
+                {/* </Link> */}
             </span>
         </CookieConsent>
     );
